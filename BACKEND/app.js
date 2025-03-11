134240a2f3231c34269f8d2f76dbc7be29a7d208
VinukiCharithma <nottherealvinuki@gmail.com>
2025-03-11 10:01:22 +0530

    3/11 - 1

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,1 +1,17 @@
 console.log("bye");
+//db password: Vinux1218
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+//connnecting middleware
+app.use("/",(req, res, next) => {
+    res.send("It is working he he he ");
+})
+
+mongoose.connect("mongodb+srv://admin:Vinux1218@cluster0.xeiyk.mongodb.net/")
+.then(() => console.log("Connected to MongoDB"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err) => console.log((err)));
