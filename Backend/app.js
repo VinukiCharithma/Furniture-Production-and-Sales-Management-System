049c5a93b5ba0d64010c8bf03302311b5f005ab3
vishmi11 <vishmibhagya886@gmail.com>
2025-03-18 13:16:07 +0530

    Added bulk upload and export features

--- Backend/app.js
+++ Backend/app.js
@@ -4,14 +4,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const router = require("./routes/ProductRoute");
+const exportRoutes = require("./routes/exportRoutes");
 
 const app = express();
 const cors = require("cors");
 
+
 //Middleware
 app.use(express.json());
-app.use("/products", router);
 app.use(cors());
+app.use('/api',exportRoutes);
+app.use("/products", router);
+
+
+
+// Start the server
+app.listen(5002, () => {
+    console.log("Server is running on port 5002");
+});
+
 
 mongoose.connect("mongodb+srv://admin:oIX3igYjNxllEW5u@cluster0.hkzll.mongodb.net/")
 .then(()=> console.log("Connected to mongo db"))
--- Backend/routes/ProductRoute.js
+++ Backend/routes/ProductRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");  // To handle file upload
 
 //insert model
 const Product = require("../models/ProductModel");
@@ -7,11 +8,20 @@ const Product = require("../models/ProductModel");
 //insert product controller
 const ProductController = require("../controllers/ProductController");
 
+const bulkUploadRoute = require('./BulkUploadRoute'); // Correct import
+
+
+// Add the bulk upload route
+router.post('/bulk-upload', bulkUploadRoute); // This will point to BulkUploadRoute.js
+
+
+
 router.get("/",ProductController.getAllProducts);
 router.post("/",ProductController.addProduct);
 router.get("/:id",ProductController.getById);
 router.put("/:id",ProductController.updateProduct);
 router.delete("/:id",ProductController.deleteProduct);
+
 
 //export
 module.exports = router;
